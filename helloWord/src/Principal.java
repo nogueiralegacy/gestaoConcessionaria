baca9efa6e7559305657ab393b9f21fc816bc0ac
Daniel Nogueira <daniel.nogueira.dacosta@gmail.com>
2022-08-11 21:39:07 -0300

    Mudei a branch teste

--- helloWord/src/Principal.java
+++ helloWord/src/Principal.java
@@ -1,5 +1,6 @@
 public class Principal {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
         System.out.println("Hello, World!");
+        System.out.println("MUdei");
     }
 }
