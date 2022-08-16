54594a53635b4c192ba4a29453d4daa3b01c2dc1
Daniel Nogueira <daniel.nogueira.dacosta@gmail.com>
2022-08-16 19:46:45 -0300

    Adição do toString para as classes Pessoa e Empresa, expressa os dados necessários no contrato dessas entidades

--- projeto/src/entities/Empresa.java
+++ projeto/src/entities/Empresa.java
@@ -1,7 +1,7 @@
 package entities;
 
 // TODO documentar
-public class Empresa extends Papel {
+public class Empresa {
     protected String nome;
     protected Contato contato;
     protected Endereco endereco;
@@ -37,5 +37,22 @@ public class Empresa extends Papel {
 
     public void setCnpj(String cnpj) {
         this.cnpj = cnpj;
+    }
+
+    public String toString() {
+        String dadosEmpresa = null;
+
+        dadosEmpresa += getNome() + ",";
+        dadosEmpresa += ", portador do cpnj n° " + getCnpj();
+        dadosEmpresa += ", endereço da sede da empresa na rua: " + getEndereco().getRua();
+        dadosEmpresa += ", n°: " + getEndereco().getNumero();
+        dadosEmpresa += ", bairro: " + getEndereco().getBairro();
+        dadosEmpresa += ", cep: " + getEndereco().getCep();
+        dadosEmpresa += ", cidade: " + getEndereco().getCidade();
+        dadosEmpresa += ", estado: " + getEndereco().getEstado() + ".\n";
+        dadosEmpresa += "Telefone: " + getContato().getTelefone();
+        dadosEmpresa += "Email: " + getContato().getEmail();
+
+        return dadosEmpresa;
     }
 }
