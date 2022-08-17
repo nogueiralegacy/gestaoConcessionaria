44c744c2111a1a2f6f191ff984081e4038fde182
Daniel Nogueira <daniel.nogueira.dacosta@gmail.com>
2022-08-16 22:41:32 -0300

    Arquivos complementares, casos de teste

---
+++ projeto/src/entities/SistemaDeArquivosVeiculoRepository.java
@@ -0,0 +1,51 @@
+package entities;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.BufferedReader;
+
+public class SistemaDeArquivosVeiculoRepository implements VeiculoRepository {
+
+    @Override
+    public Veiculo getVeiculoPlaca(String placaParaBuscar) throws IOException {
+        FileReader r = null;
+        BufferedReader bf = null;
+        String arquivo = "C:\\Users\\danie\\Documents\\gestaoConcessionaria\\projeto\\arquivos\\veiculos.txt";
+
+        try {
+            r = new FileReader(arquivo);
+            bf = new BufferedReader(r);
+
+            String linha;
+            String[] campos = new String[11];
+
+            while ((linha = bf.readLine()) != null) {
+                campos = linha.split(";");
+                if (campos[8].equals(placaParaBuscar)) {
+                    String modelo = campos[0];
+                    int ano = Integer.parseInt(campos[1]);
+                    String marca = campos[2];
+                    double valorComercial = Double.parseDouble(campos[3]);
+                    String chassi = campos[4];
+                    String cor = campos[5];
+                    int kilometragem = Integer.parseInt(campos[6]);
+                    String renavam = campos[7];
+                    String placa = campos[8];
+                    String categoria = campos[9];
+                    String adicionais = campos[10];
+
+                    return new Veiculo(modelo, ano, marca, valorComercial, chassi, cor, kilometragem,
+                            renavam, placa, categoria, adicionais);
+                }
+
+            }
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            r.close();
+            bf.close();
+        }
+
+        return null;
+    }
+}
