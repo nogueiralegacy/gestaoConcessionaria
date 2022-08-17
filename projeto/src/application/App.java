d49b07f33411100ab4b9e906e2146b35fc899432
Daniel Nogueira <daniel.nogueira.dacosta@gmail.com>
2022-08-17 20:11:12 -0300

    Organizacao dos repositorios, criacao do sistema de arquivos para cliente

--- projeto/src/application/App.java
+++ projeto/src/application/App.java
@@ -1,24 +1,35 @@
 package application;
 
-import entities.ColaboradorVendedor;
-import entities.ColaboradorVendedorRepository;
-import entities.SistemaDeArquivosColaboradorVendedorRepository;
-import entities.SistemaDeArquivosVeiculoRepository;
-import entities.Veiculo;
-import entities.VeiculoRepository;
+import entities.grupamentoCliente.Cliente;
+import entities.grupamentoCliente.grupamentoPessoa.ClienteRepository;
+import entities.grupamentoColaboradorVendedor.ColaboradorVendedor;
+import entities.grupamentoColaboradorVendedor.ColaboradorVendedorRepository;
+import entities.grupamentoVeiculo.Veiculo;
+import entities.grupamentoVeiculo.VeiculoRepository;
+import entities.sistemaDeArquivos.SistemaDeArquivosClienteRepository;
+import entities.sistemaDeArquivos.SistemaDeArquivosColaboradorVendedorRepository;
+import entities.sistemaDeArquivos.SistemaDeArquivosVeiculoRepository;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println();
+        // @Teste SistemaDeArquivosVeiculoRepository;
         String placa = "PRW6D02";
         VeiculoRepository repoVei = new SistemaDeArquivosVeiculoRepository();
         Veiculo veiculo = repoVei.getVeiculoPlaca(placa);
         System.out.println(veiculo.isUsado());
 
+        // @Teste SistemaDeArquivosColaboradorVendedorRepository;
         int id = 001;
         ColaboradorVendedorRepository repoCola = new SistemaDeArquivosColaboradorVendedorRepository();
         ColaboradorVendedor colaboradorVendedor = repoCola.getColaboradorVendedorId(id);
         System.out.println(colaboradorVendedor.getSalarioBase());
-        System.out.println("Hello, World!");
+
+        // @Teste SistemaDeArquivosClienteRepository;
+        String cpf = "020672779-12";
+        ClienteRepository repoCliente = new SistemaDeArquivosClienteRepository();
+        Cliente cliente = repoCliente.getClienteCpf(cpf);
+        String estadoCivil = String.valueOf(cliente.getEstadoCivil()).toLowerCase();
+        System.out.println(estadoCivil);
+        System.out.println("APROVADO");
     }
 }
--- projeto/src/entities/ColaboradorVendedorRepository.java
+++
@@ -1,7 +0,0 @@
-package entities;
-
-import java.io.IOException;
-
-public interface ColaboradorVendedorRepository {
-    public ColaboradorVendedor getColaboradorVendedorId(int idParaBuscar) throws IOException;
-}
