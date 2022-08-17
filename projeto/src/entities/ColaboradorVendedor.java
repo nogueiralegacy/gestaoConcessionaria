c76ae9f44cf50a206a59b8ca4c5514e87703bd64
Daniel Nogueira <daniel.nogueira.dacosta@gmail.com>
2022-08-16 23:38:32 -0300

    Faturamento não é um atributo de ColaboradorVendedor

--- projeto/src/entities/ColaboradorVendedor.java
+++ projeto/src/entities/ColaboradorVendedor.java
@@ -4,14 +4,12 @@ package entities;
 public class ColaboradorVendedor extends Pessoa {
     private double salarioBase;
     private int id;
-    private double faturamentoTotal;
 
     public ColaboradorVendedor(String nome, Contato contatoPessoal, Endereco enderecoResidencial,
             EstadoCivil estadoCivil, String cpf,
             String rg, double salarioBase, int id) {
         super(nome, contatoPessoal, enderecoResidencial, estadoCivil, cpf, rg);
         this.salarioBase = salarioBase;
-        this.faturamentoTotal = 0;
         this.id = id;
     }
 
@@ -29,13 +27,5 @@ public class ColaboradorVendedor extends Pessoa {
 
     public void setSalarioBase(double salarioBase) {
         this.salarioBase = salarioBase;
-    }
-
-    public double getFaturamentoTotal() {
-        return faturamentoTotal;
-    }
-
-    public void setFaturamentoTotal(double faturamentoTotal) {
-        this.faturamentoTotal = faturamentoTotal;
     }
 }
