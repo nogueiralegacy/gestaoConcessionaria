ff680388acc5707df14d2514875d1757e468f3d8
Daniel Nogueira <daniel.nogueira.dacosta@gmail.com>
2022-08-18 10:21:00 -0300

    Organização dos repositorios do sistema de arquivos/sistema Repository

---
+++ projeto/src/entities/grupamentoConcessionaria/ConcessionariaRepository.java
@@ -0,0 +1,7 @@
+package entities.grupamentoConcessionaria;
+
+import java.io.IOException;
+
+public interface ConcessionariaRepository {
+    public Concessionaria getIntance() throws IOException;
+}
