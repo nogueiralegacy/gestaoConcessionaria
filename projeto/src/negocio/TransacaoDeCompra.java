f480faef88ea04735841d991f06e7665b27d90e9
Daniel Nogueira <daniel.nogueira.dacosta@gmail.com>
2022-08-19 15:23:14 -0300

    Atributo dataHora foi alterado no contrutor para marcar o momento em que Transacao é criada

--- projeto/src/negocio/TransacaoDeCompra.java
+++ projeto/src/negocio/TransacaoDeCompra.java
@@ -1,11 +1,9 @@
 package negocio;
 
-import java.time.LocalDateTime;
-
 public class TransacaoDeCompra extends Transacao {
 
     public TransacaoDeCompra(Cliente cliente, Concessionaria concessionaria, Veiculo veiculo, TransacaoStatus status,
-            LocalDateTime dataHora, double montante, long id) {
-        super(cliente, concessionaria, veiculo, status, dataHora, montante, id);
+            double montante, long id) {
+        super(cliente, concessionaria, veiculo, status, montante, id);
     }
 }
